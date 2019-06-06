8b77f4d8e07f6cf873e6cffda6e331e647feb0d9
Abedin Keramati <keramatiabi@gmail.com>
2019-06-06 16:11:08 +0430

    frist version pics

---
+++ src/components/searchBar.js
@@ -0,0 +1,17 @@
+import  React from 'react';
+
+
+class searchBar extends React.Component
+{
+
+    render (){
+        return (
+            <div>
+                <form>
+                    <input />
+                </form>
+            </div>
+        );
+        };
+};
+export default searchBar;
